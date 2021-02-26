6e3e1c1fa1c7e12e57903ee0269aa89f3fc64985
Kirill Ushnurtsev <ushnurtsev@gmail.com>
2021-02-26 12:01:59 +0400

    tic tac toe

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,42 +2,9 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import * as Utils from '../utils'
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
-
+function Board({squares, selectSquare}) {
   function renderSquare(i) {
     return (
       <button className="square" onClick={() => selectSquare(i)}>
@@ -48,8 +15,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -65,18 +30,68 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
+function History({steps, currentStep, setCurrentStep}) {
+  const moves = steps.map(step => {
+    const desc = step ? `Go to move #${step}` : 'Go to game start'
+    const isCurrentStep = step === currentStep
+    return (
+      <li key={step}>
+        <button disabled={isCurrentStep} onClick={() => setCurrentStep(step)}>
+          {desc} {isCurrentStep ? '(current)' : null}
+        </button>
+      </li>
+    )
+  })
+  return (
+    <React.Fragment>
+      <ol>{moves}</ol>
+    </React.Fragment>
+  )
+}
+
 function Game() {
+  const [history, setHistory] = Utils.useLocalStorageState('history:key', [Array(9).fill(null)])
+  const [currentStep, setCurrentStep] = Utils.useLocalStorageState('history:step', 0)
+
+  const currentSquares = history[currentStep]
+  const winner = calculateWinner(currentSquares)
+  const nextValue = calculateNextValue(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  function selectSquare(square) {
+
+    if (winner || currentSquares[square]) {
+      return;
+    }
+
+    const newSquares = [...currentSquares]
+    newSquares[square] = nextValue
+    const newHistory = history.slice(0, currentStep + 1)
+
+    setHistory([...newHistory, newSquares])
+    setCurrentStep(newHistory.length)
+  }
+
+  function restart() {
+    setHistory([Array(9).fill(null)])
+    setCurrentStep(0)
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+        <Board squares={currentSquares} selectSquare={selectSquare}/>
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <History steps={history.map((h, i) => i)} currentStep={currentStep} setCurrentStep={setCurrentStep} />
       </div>
     </div>
   )
